e3432b38e190902876061fd0c1c2b08812ab3406
dalbay <fadalbay@gmail.com>
2019-09-15 01:52:34 -0400

    adding signup/login display

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,20 +1,14 @@
-/**Global variables */
+
+
+/*Login button - Displays Login Modal*/
 var btnLogin = document.getElementById("btnLogin").addEventListener('click',function(){modalLogin.style.display='block'});
-var btnSignup = document.getElementById("btnSignup").addEventListener('click',function(){modalSignup.style.display='block'});
 var modalLogin = document.getElementById("loginModal");
+
+/*SignUp button - Displays Signup Modal */
 var modalSignup = document.getElementById("signupModal");
+var btnSignup = document.getElementById("btnSignup").addEventListener('click',function(){modalSignup.style.display='block'});
 
+/*Hide the Modal*/
+document.getElementById('loginClose').addEventListener('click',()=>{modalLogin.style.display='none'});
+document.getElementById('signupClose').addEventListener('click',()=>{modalSignup.style.display='none'});
 
-
-
-// /**function that displays the modal */
-// function displaysModal(){
-//     modal.style.display = block;
-// }
-
-// btnLogin.onclick = function(){   
-//     modal.style.display = 'block';
-// }
-
-/**Event handlers */
-//btnLogin.addEventListener('click', displaysModal());
