54fdbe502ed9b7265f94e994b7390c130bce8dcb
dalbay <fadalbay@gmail.com>
2019-09-14 23:34:03 -0400

    css js html modified

---
+++ Scripts/app.js
@@ -0,0 +1,20 @@
+/**Global variables */
+var btnLogin = document.getElementById("btnLogin").addEventListener('click',function(){modalLogin.style.display='block'});
+var btnSignup = document.getElementById("btnSignup").addEventListener('click',function(){modalSignup.style.display='block'});
+var modalLogin = document.getElementById("loginModal");
+var modalSignup = document.getElementById("signupModal");
+
+
+
+
+// /**function that displays the modal */
+// function displaysModal(){
+//     modal.style.display = block;
+// }
+
+// btnLogin.onclick = function(){   
+//     modal.style.display = 'block';
+// }
+
+/**Event handlers */
+//btnLogin.addEventListener('click', displaysModal());
