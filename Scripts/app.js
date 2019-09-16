c204f52a139dbe398c6ebdd4902fa3300c869146
dalbay <fadalbay@gmail.com>
2019-09-15 23:08:25 -0400

    ux design final commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -12,3 +12,31 @@ var btnSignup = document.getElementById("btnSignup").addEventListener('click',fu
 document.getElementById('loginClose').addEventListener('click',()=>{modalLogin.style.display='none'});
 document.getElementById('signupClose').addEventListener('click',()=>{modalSignup.style.display='none'});
 
+
+
+// Close app button
+function redirectToIndex(){
+    window.location = "index.html";
+}
+
+
+function myFunction() {
+    var x = document.title;
+    switch(x){
+
+                case "New Time Sheet":
+            document.getElementById('newTimeSheet').style.backgroundColor= '#3a8668';
+                break;
+        case "Current Time Sheet":
+            document.getElementById('currentTimeSheet').style.backgroundColor= '#3a8668';
+              break;
+
+        case 'Personal Info':
+                document.getElementById('personalInfo').style.backgroundColor= '#3a8668';
+                break;
+        case 'Previous Time Sheet' :
+                document.getElementById('previousTimeSheet').style.backgroundColor= '#3a8668';
+    }
+  }
+
+  document.querySelector("body").addEventListener('load',myFunction);
